9e274067bfcc8eaa1cff2033c2946c37d729aa4e
cat <supercutcat@outlook.com>
2021-12-05 02:26:06 +0800

    Test naive ui components

--- movieui/src/App.vue
+++ movieui/src/App.vue
@@ -1,21 +1,36 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <n-config-provider
+    :theme="theme"
+    :theme-overrides="{ common: { fontWeightStrong: '600' } }"
+  >
+    <n-message-provider>
+      <n-card>
+        <n-space>
+          <n-button @click="theme = darkTheme">深色</n-button>
+          <n-button @click="theme = null">浅色</n-button>
+        </n-space>
+        <br />
+        <Login />
+      </n-card>
+    </n-message-provider>
+  </n-config-provider>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script >
+import { defineComponent, ref } from "vue";
+import { darkTheme } from "naive-ui";
+import Login from "./views/Login.vue";
+export default defineComponent({
+  setup() {
+    return {
+      darkTheme,
+      theme: ref(null),
+    };
+  },
+  components: {
+    Login,
+  },
+});
+</script>
+
+<style></style>
--- movieui/src/main.js
+++ movieui/src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import naive from "naive-ui";
 
-createApp(App).mount('#app')
+// 通用字体
+import "vfonts/Lato.css";
+// 等宽字体
+import "vfonts/FiraCode.css";
+
+createApp(App).use(naive).mount("#app");
