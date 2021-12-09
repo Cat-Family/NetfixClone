190661fed1c0390e6f5e747f24d5b01e70e12ed6
zhuyi <1293453296@qq.com>
2021-12-09 08:54:28 +0800

    登录注销接口

--- movie/src/main/java/com/yill/controller/UserController.java
+++ movie/src/main/java/com/yill/controller/UserController.java
@@ -1,15 +1,28 @@
 package com.yill.controller;
 
 
+import cn.hutool.core.lang.Assert;
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.crypto.SecureUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.yill.common.dto.LoginDto;
+import com.yill.entity.User;
+import com.yill.service.UserService;
+import com.yill.utils.JwtUtils;
 import com.yill.utils.Result;
 import com.yill.entity.dto.user.input.UserAddInput;
 import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 /**
@@ -24,12 +37,39 @@ import javax.validation.Valid;
 @RequestMapping("/user")
 public class UserController {
 
-    @ApiOperation(value = "注册功能", tags = { "登录" })
-    @RequestMapping(value = "/add", produces = { "application/json" }, method = RequestMethod.POST)
-    @Valid
-    public Result add(@RequestBody UserAddInput input) throws Exception {
 
-        return  null;
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @ApiOperation(value = "注册功能", tags = { "登录" })
+    @RequestMapping(value = "/login", produces = { "application/json" }, method = RequestMethod.POST)
+    @Valid
+    public Result login(@Validated @RequestBody LoginDto loginDto, HttpServletResponse response) {
+        User user = userService.getOne(new QueryWrapper<User>().eq("username", loginDto.getUsername()));
+        Assert.notNull(user, "用户不存在");
+        if(!user.getPassword().equals(SecureUtil.md5(loginDto.getPassword()))) {
+            return Result.fail("密码错误！");
+        }
+        String jwt = jwtUtils.generateToken(user.getId());
+        response.setHeader("Authorization", jwt);
+        response.setHeader("Access-Control-Expose-Headers", "Authorization");
+        // 用户可以另一个接口
+        return Result.succ(MapUtil.builder()
+                .put("id", user.getId())
+                .put("username", user.getName())
+                .put("email", user.getEmail())
+                .map()
+        );
     }
 
+    // 退出
+    @RequestMapping("/logout")
+    @RequiresAuthentication
+    public Result logout() {
+        SecurityUtils.getSubject().logout();
+        return Result.succ(null);
+    }
 }
