7594c43f98f9871f92c997e3165252b376df818d
zhuyi <1293453296@qq.com>
2022-01-19 12:52:19 +0800

    验证码校验

--- movie/src/main/java/com/yill/controller/UserController.java
+++ movie/src/main/java/com/yill/controller/UserController.java
@@ -56,7 +56,7 @@ public class UserController {
     @ApiOperation(value = "登录功能", tags = {"用户"})
     @RequestMapping(value = "/login", produces = {"application/json"}, method = RequestMethod.POST)
     @Valid
-    public Result login( @RequestBody LoginDto loginDto, HttpServletResponse response) {
+    public Result login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
         QueryWrapper<User> queryWrapper = new QueryWrapper<User>();
         queryWrapper.and(wrapper -> wrapper
                 .eq("name", loginDto.getName()).or()
@@ -75,46 +75,56 @@ public class UserController {
                 .put("id", user.getId())
                 .put("username", user.getName())
                 .put("email", user.getEmail())
-                .put("token",jwt)
+                .put("token", jwt)
                 .map()
         );
     }
-    @ApiOperation(value = "郵箱登录功能", tags = {"用户"})
+
+    @ApiOperation(value = "获取验证码", tags = {"用户"})
     @RequestMapping(value = "getCheckCode", produces = {"application/json"}, method = RequestMethod.POST)
     @ResponseBody
-    public String getCheckCode(String email,String code){
-        System.out.println("邮箱登录");
+    public String getCheckCode(String email) {
         String checkCode = String.valueOf(new Random().nextInt(899999) + 100000);
-        redisUtils.setWithTime(email,checkCode,60);
-        String message = "欢迎使用无花果影音，您的注册验证码为："+checkCode;
+        redisUtils.setWithTime(email, checkCode, 60);
+        String message = "欢迎使用无花果影音，您的注册验证码为：" + checkCode;
         try {
             mailService.sendSimpleMail(email, "注册验证码", message);
-            User user = new User();
-            user.setEmail(email);
-            user.setName(email);
-            if (!code.equals(checkCode)){
-                Result.fail("验证码错误");
-            }
-            userMapper.insert(user);
-        }catch (Exception e){
+            redisUtils.set("code", checkCode);
+        } catch (Exception e) {
             return "";
         }
         return checkCode;
     }
 
+    @ApiOperation(value = "邮箱登录", tags = {"用户"})
+    @RequestMapping(value = "email-login", produces = {"application/json"}, method = RequestMethod.POST)
+    @ResponseBody
+    public Result emailLogin(String validateCode) {
+        if (validateCode == null) {
+            return Result.fail("错误获取");
+        }
+        //redis中的验证码
+        Object checkCode = redisUtils.get("code");
+        //校验
+        System.out.println(checkCode);
+        if (validateCode != null && validateCode.equals(checkCode)) {
+            return Result.succ("验证码正确");
+        }
+        return Result.fail("验证码错误");
+    }
 
 
     @ApiOperation(value = "注册功能", tags = {"用户"})
     @RequestMapping(value = "register", produces = {"application/json"}, method = RequestMethod.POST)
     @ResponseBody
-    public Result region(@RequestBody RegisterDto registerDto){
-       return userService.register(registerDto);
+    public Result region(@RequestBody RegisterDto registerDto) {
+        return userService.register(registerDto);
     }
 
     @ApiOperation(value = "修改完善功能", tags = {"用户"})
     @RequestMapping(value = "update", produces = {"application/json"}, method = RequestMethod.POST)
     @ResponseBody
-    public void modify(ModifyDto modifyDto){
+    public void modify(ModifyDto modifyDto) {
         userService.modify(modifyDto);
     }
 
