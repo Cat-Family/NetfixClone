cee0816734555e09d0daf85fa0d3ec52ac27899a
cat <supercutcat@outlook.com>
2022-01-21 17:12:59 +0800

    feat some future

--- movieui/src/pages/Watch/Watch.vue
+++ movieui/src/pages/Watch/Watch.vue
@@ -1,11 +1,11 @@
 <template>
   <video
     class="home-video"
-    src="/test2.mp4"
+    src="/crown.mp4"
     preload="auto"
     type="video/mp4"
     controls
-    poster="/website.jpg"
+    poster="/login-the-crown_2-1500x1000.jpg"
     style="height: 99vh; width: 99vw"
   ></video>
 </template>
