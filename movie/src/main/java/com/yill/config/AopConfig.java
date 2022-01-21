7240bc6e8a063e9dbf89fbc7f44e46b09107cb0a
LiuLin <jiahaoprogrammer@outlook.com>
2022-01-21 11:26:17 +0800

    Fix LogAop

--- movie/src/main/java/com/yill/config/AopConfig.java
+++ movie/src/main/java/com/yill/config/AopConfig.java
@@ -2,7 +2,10 @@ package com.yill.config;
 
 import com.yill.utils.RedisUtils;
 import com.yill.utils.Result;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
@@ -12,7 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Component
 @Aspect
@@ -24,14 +29,17 @@ public class AopConfig {
     @Pointcut("execution(public * com.yill.controller..*.*(..))")
     public void logRecordAop() {}
 
+    @Pointcut("execution(public * com.yill.controller..*.*(..))")
+    public void exceptionLogRecordAop() {}
+
     @Around("logRecordAop()")
-    public Object around(ProceedingJoinPoint pjd) throws Throwable {
+    public Object logRecord(ProceedingJoinPoint pjd) throws Throwable {
         String className = pjd.getSignature().getClass().getName();
         String methodName = pjd.getSignature().getName();
         Logger log = LoggerFactory.getLogger(className.getClass());
         Object result = null;
+        Date date = new Date();
         long start = System.currentTimeMillis();
-        Date date = new Date(start);
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         SimpleDateFormat logSimpleDateFormat = new SimpleDateFormat("yyyyMMdd");
         String format = simpleDateFormat.format(date);
@@ -45,10 +53,27 @@ public class AopConfig {
         log.info("接口"+methodName + params);
         result = pjd.proceed();
         String resultString = result.toString();
-        redisUtils.setList("log"+logFormat,"请求时间:"+format+",接口"+methodName + params+",返回报文"+resultString+"，接口"+methodName + "执行时长:" + (System.currentTimeMillis() - start+"ms"));
+        redisUtils.setList("log"+logFormat,"==============请求时间:"+format+",接口"+methodName + params+",返回报文"+resultString+"，接口"+methodName + "执行时长:" + (System.currentTimeMillis() - start+"ms"));
         log.info("接口"+methodName +"返回报文:" + (result instanceof Result ? (Result) result : result));
         log.info("接口"+methodName+"请求时间:"+format);
         log.info("接口"+methodName + "执行时长:" + (System.currentTimeMillis() - start+"ms"));
         return result;
+    }
+
+    @AfterThrowing(value = "exceptionLogRecordAop()",throwing = "throwable")
+    public void exceptionLogRecord(JoinPoint jp, Throwable throwable) throws Throwable {
+        List<Object> list = new ArrayList<>();
+        Date date = new Date();
+        long start = System.currentTimeMillis();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat logSimpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+        String logFormat = logSimpleDateFormat.format(date);
+        String format = simpleDateFormat.format(date);
+        Signature signature = jp.getSignature();
+        Object[] args = jp.getArgs();
+        for (Object arg : args) {
+            list.add(arg);
+        }
+        redisUtils.setList("elog"+logFormat,"==============请求时间:"+format+",接口"+signature.toString()+"异常"+",请求参数:"+list+",异常信息为:"+throwable+",执行时长:" + (System.currentTimeMillis() - start+"ms"));
     }
 }
